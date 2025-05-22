10bf62db607970bde446b3625c511b4e29f9f7f2
Bernard <romeo.bernard@ynov.com>
2025-05-22 15:02:49 +0200

    connect to db

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # php-Ecommerce
 
 Benjamin GOTTRANT - Romeo BERNARD
+
+# ".env" --> exemple
+
+DB_HOST="your host"
+DB_NAME="your db name"
+DB_USER="username"
+DB_PASSWORD="password"
--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,8 +1,29 @@
 <?php
-$mdp = getenv('PHPMYADMIN_MDP') ;
-$mysqli = new mysqli("localhost", "root", "Root" , "php-Ecommerce");
 
-if ($mysqli->connect_errno) {
-    die("Erreur de connexion : " . $mysqli->connect_error);
+function loadEnv($path) {
+    if (!file_exists($path)) return;
+
+    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (str_starts_with(trim($line), '#')) continue;
+
+        list($key, $value) = explode('=', $line, 2);
+        $key = trim($key);
+        $value = trim($value);
+        if (!array_key_exists($key, $_ENV)) {
+            $_ENV[$key] = $value;
+        }
+    }
+}
+
+loadEnv(__DIR__ . '/../.env');
+var_dump($_ENV);
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie !";
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
 }
 ?>
